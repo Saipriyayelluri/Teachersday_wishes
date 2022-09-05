2ceefda52ae820735bf91d2b4bc9fab53d35d589
Saipriya yelluri <110100728+Saipriyayelluri@users.noreply.github.com>
2022-09-05 12:57:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import tkvideo
+root = Tk()
+root.state('zoom')
+# root.call(__command=karuna_sir())
+my_label = Label(root)
+my_label.pack()
+player = tkvideo.tkvideo("C:\\Users\\KATTAKART\\Downloads\\Teachers Day Video Presentation Download-(BestStatusVideo.com).mp4", my_label, loop = 1, size = (1280,720))
+player.play()
+root.mainloop()
+
+
+
+
+
