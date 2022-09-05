d254fd5c7f3bceae06faf53282438afa8ca4cd2e
Saipriya yelluri <110100728+Saipriyayelluri@users.noreply.github.com>
2022-09-05 13:32:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from tkinter import *
 import tkvideo
+from pygame import mixer
+mixer.init()
+#loading the song
+mixer.music.load("Teachers_Day__Flute__Bgm__Theme.mp3")
+#playing the audio
+mixer.music.play(-1)
 root = Tk()
 root.state('zoom')
 # root.call(__command=karuna_sir())
 my_label = Label(root)
 my_label.pack()
+
 player = tkvideo.tkvideo("Teachers Day Video Presentation Download-(BestStatusVideo.com).mp4", my_label, loop = 1, size = (1280,720))
 player.play()
 root.mainloop()
-
 
 
 
